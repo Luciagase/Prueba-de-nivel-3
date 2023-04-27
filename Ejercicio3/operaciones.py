c944cf816e23cf11dde6b64a63394f37611ce9fd
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-04-27 22:13:19 +0200

    operaciones ej3

---
+++ Ejercicio3/operaciones.py
@@ -0,0 +1,31 @@
+def suma(a,b):
+    try:
+        r=a+b
+    except TypeError:
+        print("Error: Tipo de dato no válido")
+    else:
+        return r
+
+def resta(a,b):
+    try:
+        r=a-b
+    except TypeError:
+        print("Error: Tipo de dato no válido")
+    else:
+        return r
+def multiplicacion(a,b):
+    try:
+        r=a*b
+    except TypeError:
+        print("Error: Tipo de dato no válido")
+    else:
+        return r
+def division(a,b):
+    try:
+        r=a/b
+    except TypeError:
+        print("Error: Tipo de dato no válido")
+    except ZeroDivisionError:
+        print("Error: No es posible dividir por cero")
+    else:
+        return r
